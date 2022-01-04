b63b0f67dd6a9b0a905ee5bce922667a9c8a6375
xamata <61128570+xamata@users.noreply.github.com>
2022-01-03 17:29:04 -0800

    Update conwaysGameofLife.py

--- conwaysGameofLife.py
+++ conwaysGameofLife.py
@@ -6,112 +6,74 @@ Rules:
 2. Empty square = 'dead'
 3. if 'alive' sqaure has 2 or 3 neighbors(touching, corner squares included)
 	it will continue to live
-4. if 'dead' square has 3 neighbors, it becomes 'alive'
+4. if 'de#mes 'alive'
 5. every other 'alive' square will become 'dead'
 '''
 
-import random, copy, time
-# need to create a random scenario for the game to start
-boardWidth = 150
+# need to create a random board for the game
+import random,copy, time
+
+boardWidth = 100
 boardHeight = 20
 
-nextCells = [] # stores the current boards cell information
+nextSquare = []
 for x in range(boardWidth):
 	column = []
-	for y in range(boardHeight): 
-		if random.randint(0,1) == 0:
-			column.append('#') # adds a living cell
-		else: 
-			column.append(' ') # adds a dead cell
-	nextCells.append(column)
-
-# create the game within a While loop
-while True:
-	print('\n\n\n\n\n') # separating the different boards
-	currentCells = copy.deepcopy(nextCells)
-
-	# print current cells on the screen
 	for y in range(boardHeight):
-		for x in range(boardWidth):
-			print(nextCells[x][y], end='') # print the # or space
-		print() # print a newline at the end of the row.
+		# create a single glider with: 'if (x, y) in ((1, 0), (2, 1), (0, 2), (1, 2), (2, 2)):'
+		if random.randint(0,1) == 0: # sqaure is alive
+			column.append('#')
+		else:
+			column.append(' ') # sqaure is dead
+	nextSquare.append(column) # nextSqaure is a list of column lists
 
-	# Cacluate the next step's cells based on current step's cells:
+# create the main code for the game
+while True:
+	# creating the game board
+	print('\n\n\n\n')
+	currentSquare = copy.deepcopy(nextSquare)
+	for y in range(boardHeight): # for loop begins with y bc of how command line prints horizontally first
+		for x in range(boardWidth):
+			print(currentSquare[x][y], end='')
+		print()
+
+	# create the parameters for the game rules, checking the filled in boxes around each box
+	# we have x and y, we need the surrounding coordinates:
 	for x in range(boardWidth):
 		for y in range(boardHeight):
-			# Get the neighboring coordinates: 
+			# we add/subtract depending on left or right
+			# the % ensures leftCoord is always between 0 and Width-1
+			# if it was (0-1) = -1 , that needs to be within 0 and boardWidth
+			# mod will make it 99 (this is a mod wraparound technique)
 			leftCoord = (x-1) % boardWidth
 			rightCoord = (x+1) % boardWidth
-			aboveCoord = (y-1) % boardHeight
-			belowCoord = (y+1) % boardHeight
+			aboveCoord = (y+1) % boardHeight
+			belowCoord = (y-1) % boardHeight 
 
-			# Count number of living neighbors:
-			numNeighbors = 0
-			if currentCells[leftCoord][aboveCoord] == '#':
-				numNeighbors += 1 # Top-left neighbor is alive.
-			if currentCells[x][aboveCoord] == '#':
-				numNeighbors += 1 # Top neighbor is alive.
-			if currentCells[rightCoord][aboveCoord] == '#':
-				numNeighbors += 1 # Top-right neighbor is alive.
-			if currentCells[leftCoord][y] == '#':
-				numNeighbors += 1 # Left neighbor is alive.
-			if currentCells[rightCoord][y] == '#':
-				numNeighbors += 1 # Right neighbor is alive.	
-			if currentCells[leftCoord][belowCoord] == '#':
-				numNeighbors += 1 # Bottom-Left neighbor is alive.
-			if currentCells[x][belowCoord] == '#':
-				numNeighbors += 1 # Bottom neighbor is alive.
-			if currentCells[rightCoord][belowCoord] == '#':
-				numNeighbors += 1 # Bottom-Right neighbor is alive.
+			numOfNeighbors = 0
+			if currentSquare[leftCoord][aboveCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on TopLeft
+			if currentSquare[x][aboveCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on Top	
+			if currentSquare[rightCoord][aboveCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on TopRight
+			if currentSquare[leftCoord][y] == '#':
+				numOfNeighbors += 1 # Neighbor on Left		
+			if currentSquare[rightCoord][y] == '#':
+				numOfNeighbors += 1 # Neighbor on Right	
+			if currentSquare[leftCoord][belowCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on BottomLeft
+			if currentSquare[x][belowCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on Bottom
+			if currentSquare[rightCoord][belowCoord] == '#':
+				numOfNeighbors += 1 # Neighbor on BottomRight
 
-			# set cell based on conways game of life rules
-			if currentCells[x][y] == '#' and (numNeighbors ==2 or numNeighbors==3):
-				nextCells[x][y] = '#'
-			elif currentCells[x][y] == ' ' and numNeighbors == 3:
-				nextCells[x][y] = '#'
-			else:
-				nextCells[x][y] = ' '
-	time.sleep(0.5)
+			# add or delete '#' depending on the rules of the game
+			if currentSquare[x][y] == '#' and (numOfNeighbors == 2 or numOfNeighbors == 3):
+				nextSquare[x][y] = '#'
+			elif currentSquare[x][y] == ' ' and (numOfNeighbors == 3):
+				nextSquare[x][y] = '#'
+			else: 
+				nextSquare[x][y] = ' '
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for x in range(boardWidth):
-	for y in range(boardHeight):
-		numNeighbors = 0
-		if nextCells[x][y] == '#' and (numNeighbors == 2 or numNeighbors ==3):
-			# Living cells with 2 or 3 neightbors stay alive:
-			nextCells[x][y] = '#'
-		elif nextCells[x][y] == ' ' and numNeighbors == 3:
-			# Dead cells with 3 neighbors become alive:
-			nextCells[x][y] = '#'
-		else: 
-			# Everything else dies or stays dead:
-			nextCells[x][y] = ' '
-
-# create the next cells information, where the game loops as well
-	
+	time.sleep(0.25)
